10827a95ac379a320b5907e7f8e1f1b120be990c
Florian Foezon <ffoezon@gmail.com>
2022-04-27 21:40:54 +0200

    npm librairy table created

--- src/pages/EmployeeList.js
+++ src/pages/EmployeeList.js
@@ -1,25 +1,25 @@
 import React from 'react';
-// import EmployeesTable from '../components/EmployeesTable';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
-import TableEmployee from '../components/TableEmployee';
+import { useSelector } from 'react-redux';
+import TableEmployee from 'table-plugin/dist/TableEmployee';
 /**
 * create page employee list
 *
 * @return html - page employee list
 */
 const EmployeeList = () => {
+  const rows = useSelector(state => state.employeesList);
+  const editable = rows.map(o => ({ ...o }));
   return (
     <div>
       <Header/>
       <div id="employee-div" className="container">
-        <TableEmployee/>
+        <TableEmployee data={editable}/>
       </div>
       <Footer/>
     </div>
   );
 };
-
-
 
 export default EmployeeList;
